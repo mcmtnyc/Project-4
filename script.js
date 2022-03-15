e881285c6f03cf5d3d0652210277dbb696409bbf
tmelnyc <tmelnyc@gmail.com>
2022-03-15 10:58:28 -0400

    Finished restart/start buttons sans timer

--- script.js
+++ script.js
@@ -9,6 +9,17 @@ let prevClick = 0
 /// HARD CODE ARRAY UNTIL BELOW SOLUTION
 let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 let shuffleNumArray = numArray.sort(() => Math.random() - 0.5)
+function fillButtons() {
+  for (i = 0; boardElements.length > i; i++) {
+    boardElements[i].innerHTML = `${shuffleNumArray[i]}`
+  }
+  for (let i = 0; i < boardElements.length; i++) {
+    boardElements[i].addEventListener("click", buttonClick)
+  }
+}
+
+
+
 
 /// Iterate thru to create buttons w randomized numbers... FIND WAY TO GET NO REPEATS
 // function getRandomInt(min, max) {
@@ -23,34 +34,38 @@ let shuffleNumArray = numArray.sort(() => Math.random() - 0.5)
 
 //   numArray.push(getRandomInt(1,arrayLength))
 // }
+/// Start the game onclick of start button
 
-function hitStartGameButton() {
-
-
-  for (let i = 0; i < boardElements.length; i++) {
-    boardElements[i].addEventListener("click", buttonClick)
-  }
-
-  for (let i = 0; i < boardElements.length; i++) {
-    boardElements[i].innerHTML(`${shuffleNumArray[i]}`)
-  }
-
-}
-
-
-
-// function buttonClick(event) {
-//   let currButton = event.target
-//   let currClick = currButton.innerHTML
-//   /// If X's turn
-//   if (currClick >= prevClick) {
-//     /// Disable button so that it cannot be clicked again
-//     currButton.disabled = true
-//   }
-//   /// If all buttons are disabled
-//   if (){
-//     alert.innerHTML(`DONE! Your time is ${display timerInterval}`)
-// // pause stopwatch
-//   }
+// for (let i = 0; i < boardElements.length; i++) {
+//   boardElements[i].addEventListener("click", buttonClick)
 // }
 
+
+
+
+
+function buttonClick(event) {
+  let currButton = event.target
+  let currClick = currButton.innerHTML
+
+  if (currButton > prevClick) {
+    currButton.disabled = true
+  }
+}
+
+gameButtonStart.addEventListener("click", hitStartGameButton)
+function hitStartGameButton() {
+  alert.innerHTML = `Start!`
+  fillButtons()
+}
+
+resetButton.addEventListener("click", hitResetButton)
+function hitResetButton() {
+  ///Stop the stopwatch
+  ///Store time value in var for alert
+  alert.innerHTML = `Done! Your time is: `
+  boardElements.forEach(box => {
+    box.disabled = false
+    box.innerHTML = ""
+  })
+}
